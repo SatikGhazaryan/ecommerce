071c665abacda50c94ce76298e61ae5794e1e697
SatikGhazaryan <webtrainersatik@gmail.com>
2022-07-22 12:32:35 +0400

    remove unnecessary logs

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-//console.log("ok")
 const express = require("express");
-const app = express()
-const http = require('http').createServer(app)
-const port=3003;
+const app = express();
+const http = require("http").createServer(app);
+const port = 3003;
 
+const json = express.json();
 
-//Middleware => It is those methods/functions/operations that are called BETWEEN processing the Request and sending the Response in your application method.
-const json =express.json();
+app.use(json);
 
-app.use(json)
+const staticFiles = express.static("./front");
+app.use(staticFiles);
 
- //front conect backe 
-const staticFiles = express.static('./front')
-app.use(staticFiles)
-
-
-
-http.listen(port,()=>{
-    console.log(`server is listening port ${port}`)
-})
-
+http.listen(port, () => {
+  console.log(`server is listening port ${port}`);
+});
